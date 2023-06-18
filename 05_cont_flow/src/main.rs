a3566b4431a8ec553b340204a1924b93c69e33ba
vkavsek <vkavsek@gmail.com>
2023-06-18 22:36:23 +0200

    IfControl implemented.

--- 05_cont_flow/src/main.rs
+++ 05_cont_flow/src/main.rs
@@ -5,24 +5,27 @@ fn main() {
         view! { cx, <App/> }
     })
 }
- 
-/// An example of a simple static list.
-/// Plus a demonstration of using various control flow constructs.
+
+/// An example of a simple static list, plus a demonstration of using various control flow constructs.
 #[component]
 fn App(cx: Scope) -> impl IntoView {
     let vals = (0..10).map(|idx| create_signal(cx, idx));
 
     let paragraphs = vals
-        .map(|(val, _)| {
-            let is_odd = move || val() & 1 == 1;
-
-            view! { cx, <p>{move || if is_odd() { "Odd" } else { "Even" }}</p> }
+        .map(|(value, _)| {
+            view! { cx, <IfControl value/> }
         })
         .collect_view(cx);
 
     view! { cx, <div>{paragraphs}</div> }
 }
 
+/// An if expression returns its value, and a &str implements IntoView, so a Fn() -> &str implements IntoView, so this... just works!
+/// Always remember: to be reactive, values must be functions.
+/// Note the syntax for `is_odd` closure : `{ move || if is_odd() { "Odd" } else { "Even" }}`
 #[component]
-fn IfControl(cx: Scope, value: ReadSignal<u32>) -> impl IntoView {
+fn IfControl(cx: Scope, value: ReadSignal<i32>) -> impl IntoView {
+    let is_odd = move || value() & 1 == 1;
+
+    view! { cx, <p>{move || if is_odd() { "Odd" } else { "Even" }}</p> }
 }
