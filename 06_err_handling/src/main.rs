afe0801af0316a0e6d7c8821097aad85f78c0af2
vkavsek <vkavsek@gmail.com>
2023-06-19 15:08:55 +0200

    Err handling done.

--- 06_err_handling/src/main.rs
+++ 06_err_handling/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 fn App(cx: Scope) -> impl IntoView {
     view! { cx,
         <NumericInput/>
-        <DemoErrBoundary/>
+        <BookExample/>
     }
 }
 
@@ -26,6 +26,7 @@ fn NumericInput(cx: Scope) -> impl IntoView {
     let on_input = move |ev| set_val(event_target_value(&ev).parse::<i32>());
 
     view! { cx,
+        <h2>"No error handling"</h2>
         <label>
             "Type a number (or not!)" <input type="number" on:input=on_input/>
             <p>"You entered " <strong>{val}</strong></p>
@@ -35,37 +36,47 @@ fn NumericInput(cx: Scope) -> impl IntoView {
 
 /// To actually handle the errors you can use the <ErrorBoundary/> component.
 #[component]
-fn DemoErrBoundary(cx: Scope) -> impl IntoView {
-    let (val, set_val) = create_signal(cx, Ok(0));
+fn BookExample(cx: Scope) -> impl IntoView {
+    let (value, set_value) = create_signal(cx, Ok(0));
 
-    let on_input = move |ev| set_val(event_target_value(&ev).parse::<i32>());
+    // when input changes, try to parse a number from the input
+    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
 
     view! { cx,
-        <h1>"ErrorBoundary"</h1>
+        <h2>"<ErrorBoundary/> handling"</h2>
         <label>
             "Type a number (or something that's not a number!)"
+
             <input type="number" on:input=on_input/>
-            // the fallback receives a signal containing current errors
-            <ErrorBoundary fallback=|cx, errors| {
-                view! { cx,
+            // If an `Err(_) had been rendered inside the <ErrorBoundary/>,
+            // the fallback will be displayed. Otherwise, the children of the
+            // <ErrorBoundary/> will be displayed.
+            <ErrorBoundary
+                // the fallback receives a signal containing current errors
+                fallback=|cx, errors| view! { cx,
                     <div class="error">
-                        // we can render a list of errors as strings, if we'd like
                         <p>"Not a number! Errors: "</p>
+                        // we can render a list of errors
+                        // as strings, if we'd like
                         <ul>
-                            {move || {
-                                errors
-                                    .get()
-                                    .into_iter()
-                                    .map(|(_, e)| {
-                                        view! { cx, <li>{e.to_string()}</li> }
-                                    })
-                                    .collect_view(cx)
-                            }}
+                            {move || errors.get()
+                                .into_iter()
+                                .map(|(_, e)| view! { cx, <li>{e.to_string()}</li>})
+                                .collect::<Vec<_>>()
+                            }
                         </ul>
                     </div>
                 }
-            }>
-                <p>"You entered " <strong>{val}</strong></p>
+            >
+                <p>
+                    "You entered "
+                    // because `value` is `Result<i32, _>`,
+                    // it will render the `i32` if it is `Ok`,
+                    // and render nothing and trigger the error boundary
+                    // if it is `Err`. It's a signal, so this will dynamically
+                    // update when `value` changes
+                    <strong>{value}</strong>
+                </p>
             </ErrorBoundary>
         </label>
     }
