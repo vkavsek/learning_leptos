8b4fe5aad91a5b504ad0f7d0bb19422e044e1ca5
vkavsek <vkavsek@gmail.com>
2023-06-29 17:51:14 +0200

    Added additional folder meant to demonstrate the use of collect_view() and into_view() functions.

--- 02-01_collect_view_into_view/src/main.rs
+++ 02-01_collect_view_into_view/src/main.rs
@@ -1,36 +1,30 @@
-use leptos::*;
-
-fn main() {
-    mount_to_body(|cx| view! { cx, <App/> });
-}
+use leptos::{ev::SubmitEvent, html::Input, *};
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    let names = vec!["igor", "marko", "jani", "rudi", "toni", "franci", "anton"];
+    let name_list = vec![
+        "igor", "marko", "jani", "rudi", "toni", "franci", "anton", "luka", "matija", "steven",
+        "majkolin",
+    ];
 
-    let u_names: Vec<_> = names
-        .iter()
-        .map(|name| {
-            let mut c = name.chars();
-            if let Some(ch) = c.next() {
-                let collected: String = c.collect();
-                format!("{}{}", ch, collected);
-            }
-            name
-        })
-        .collect();
+    let names: Vec<_> = uppercase_conversion(&name_list);
 
-    view! {cx,
-        <IntoViewDemo names/>
+    view! { cx,
+        <CollectViewDemo names/>
+        <IntoViewDemo/>
     }
 }
 
 #[component]
-fn IntoViewDemo<IV>(cx: Scope, names: Vec<IV>) -> impl IntoView
+fn CollectViewDemo<IV>(cx: Scope, names: Vec<IV>) -> impl IntoView
 where
     IV: IntoView,
 {
     view! { cx,
+        <h2>
+            <code>"collect_view()"</code>
+            " Demo"
+        </h2>
         <ul>
             {names
                 .into_iter()
@@ -41,3 +35,75 @@ where
         </ul>
     }
 }
+
+#[component]
+fn IntoViewDemo(cx: Scope) -> impl IntoView {
+    let (toggle, set_toggle) = create_signal(cx, false);
+
+    let click = move |_| set_toggle.update(|boolean| *boolean = !*boolean);
+    view! { cx,
+        <h2>
+            <code>"into_view()"</code>
+            " Demo"
+        </h2>
+        <button on:click=click>"Click to toggle browser input practice"</button>
+        <p>"Button is toggled: " {move || if toggle() { "On" } else { "Off" }}</p>
+        {
+            move || {
+                if toggle() {
+                    view! {cx, 
+                        <BrowserInputPractice/>
+                    }.into_view(cx)
+                } else {
+                    ().into_view(cx)
+                }
+        }
+        }
+    }
+}
+
+#[component]
+fn BrowserInputPractice(cx: Scope) -> impl IntoView {
+    let (text, set_text) = create_signal(cx, "text".to_string());
+    let input_elem = create_node_ref::<Input>(cx);
+
+    let on_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        let val = input_elem().expect("<input> to be present").value();
+        set_text(val);
+    };
+
+    let num_chars = move || text().chars().count();
+    let is_odd = move || num_chars() & 1 == 1;
+
+    view! { cx,
+        <h2>"Browser Controlled Input practice"</h2>
+        <form on:submit=on_submit>
+            <input type="text" value=text node_ref=input_elem/>
+            <input type="submit" value="Submit"/>
+        </form>
+        <p>"Submited text is: " {text}</p>
+        <p>"The number of chars is: " {num_chars}</p>
+        <p>"Which is: " {move || if is_odd() { "Odd" } else { "Even" }}</p>
+    }
+}
+
+fn main() {
+    mount_to_body(|cx| view! { cx, <App/> });
+}
+
+fn uppercase_conversion(name_list: &Vec<&str>) -> Vec<String> {
+    name_list
+        .iter()
+        .map(|&name| {
+            let mut chars_iter = name.chars();
+            let first_char = chars_iter
+                .next()
+                .expect("not to have empty Strings present")
+                .to_ascii_uppercase();
+
+            first_char.to_string() + &chars_iter.collect::<String>()
+        })
+        .collect()
+}
+
